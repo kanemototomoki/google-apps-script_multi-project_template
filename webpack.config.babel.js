1d14a611aede20313da55a679594e27d9322f3ab
kanemototomoki <kanemototomoki@gmail.com>
2021-08-01 11:10:16 +0900

    update: build js name

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -12,7 +12,7 @@ const config = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name]/bundle.js',
+    filename: '[name]/index.js',
   },
   module: {
     rules: [
